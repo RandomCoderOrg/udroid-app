0d21b1297c90ab29db9a098a67aa1529bf7c1c71
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2026-07-25 07:17:05 +0530

    chore: bump Android version to 0.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         // following the execution bridge merged into official termux-exec.
         targetSdk = 36
 
-        versionCode = 1
-        versionName = "0.0.1"
+        versionCode = 2
+        versionName = "0.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
