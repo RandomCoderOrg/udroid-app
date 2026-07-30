abe242460d0bfdaaaa6f8e51c0e79a9015aa6a13
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2026-07-30 20:43:45 +0530

    chore: release 0.0.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         // following the execution bridge merged into official termux-exec.
         targetSdk = 36
 
-        versionCode = 8
-        versionName = "0.0.8"
+        versionCode = 9
+        versionName = "0.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
