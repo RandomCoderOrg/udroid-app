4a07d1becac299948ae7b3b8cb836786636ba1ef
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2026-09-18 11:49:34 +0530

    chore(release): prepare v0.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,8 +42,8 @@ android {
         // following the execution bridge merged into official termux-exec.
         targetSdk = 36
 
-        versionCode = 12
-        versionName = "0.1.2"
+        versionCode = 13
+        versionName = "0.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
